85f7b050f7c8e9cdd412fe6e01192efe45e8a3c2
throwaway <7518647+wendychaung@users.noreply.github.com>
2022-07-21 16:57:41 +0800

    Update headTop.js

--- src/components/headTop.js
+++ src/components/headTop.js
@@ -1,12 +1,19 @@
 import React from "react";
-
 import { Container, Row, Col } from 'react-bootstrap';
+import styled from "styled-components";
+
+const Alink = styled.a`
+    text-decoration: none;
+  color: #666666;
+`
+
 
 export default function HeaderTop() {
     return <div className="header">
         <Container>
             <Row>
-                <Col className="headerTxt" md={8} xs={12}>Crypto Logos</Col>
+                <Col className="headerTxt" md={8} xs={12}>
+                    <Alink href="/">Crypto Logos</Alink></Col>
                 <Col className="headetRht" md={4} xs={12}>&copy; Web3 Camp</Col>
             </Row>
         </Container>
